8adba63632db1f830d1a64e557761c6194ddd666
lj <liangjiajiang@gmail.com>
2020-09-12 23:07:47 -0400

    public void main

--- ExprEval/src/ExprEvaluator2.java
+++ ExprEval/src/ExprEvaluator2.java
@@ -4,12 +4,12 @@ import java.util.*;
 //with decimal points supressed for whole numbers. The unrounded double value is returned.
 public class ExprEvaluator2
 {
-Scanner kb =  new Scanner(System.in);
+Scanner consoleScanner =  new Scanner(System.in);
 
 //class-wide variables:
 //Since expressions are evaluated one at a time, all evaluations can use the same two queues.
-private static Deque<Character> A = new ArrayDeque<Character>();  //Queue for operator
-private static Deque<Double> B = new ArrayDeque<Double>();
+private static Deque<Character> operatorQueue = new ArrayDeque<Character>();  //Queue for operator
+private static Deque<Double> digitQueue = new ArrayDeque<Double>();
 
 //class-wide methods:
 
@@ -37,9 +37,9 @@ performing the operation and adding the result onto B as a double value.*/
 
 private static void eval()
 {
-    char op = A.remove(); //current operator
-    double opnd1 = B.remove(); //operands numbers
-    double opnd2 = B.remove();
+    char op = operatorQueue.remove(); //current operator
+    double opnd1 = digitQueue.remove(); //operands numbers
+    double opnd2 = digitQueue.remove();
     double val = 0.0;
     switch (op) //evaluate
     {
@@ -47,16 +47,16 @@ private static void eval()
         val = opnd1 + opnd2;
         break;
       case '-':
-        val = opnd1 - opnd2;
+        val = opnd2 - opnd1;
         break;
       case '*':
         val = opnd1 * opnd2;
         break;
       case '/':
-        val = opnd1/opnd2;
+        val = opnd2/opnd1;
         break;
     }
-    B.addFirst(val); //add result onto B
+    digitQueue.addFirst(val); //add result onto B
 }
 
 /* In this method, a parenthesized subexpression is
@@ -69,10 +69,10 @@ private static void evalDown()
   do
   {
     eval();
-  }while((A.size()>0) && (A.peek() != '('));
-  if((A.size()>0) && (A.peek() == '('))
+  }while((operatorQueue.size()>0) && (operatorQueue.peek() != '('));
+  if((operatorQueue.size()>0) && (operatorQueue.peek() == '('))
   {
-    A.remove();
+    operatorQueue.remove();
   }
 }
 
@@ -101,28 +101,28 @@ private static boolean prec(char token, Deque<Character> DequeA)
 }
 
 //variables for an ExprEvaluator object
-private String e;
-private int p; //pointer to characters within the expression string e
+private String expressionInput;
+private int counter; //pointer to characters within the expression string e
 
 //constructor 
 public ExprEvaluator2()
 {
   System.out.println("enter an expression");
-  e = kb.nextLine(); //input an arithmetic expression as a line of keyboard input.
-  p = 0;      
+  expressionInput = consoleScanner.nextLine(); //input an arithmetic expression as a line of keyboard input.
+  counter = 0;      
 }
 
 //parameterized constructor
-public ExprEvaluator2(String ee)
+public ExprEvaluator2(String expressionString)
 {
-  e = ee;
-  p = 0;  
+  expressionInput = expressionString;
+  counter = 0;  
   
 }
 
 public String getExpression()
      {
-        return e;
+        return expressionInput;
      }
 
 
@@ -134,50 +134,60 @@ public String getExpression()
 private double formNum()
 {
   double total = 0.0;
-  int count = 0;
-  int flag = 0;
+  int countAfterDec = 0;
+  boolean isDecimal = false;
+  boolean isNegative = false;
   double mult = 1.0;
-  char c,d;
+  char currentDigitToken, d;
   do
   {
-    c = e.charAt(p); //examine the current character in the string (from left to right)
-    if(c == '.')
+    currentDigitToken = expressionInput.charAt(counter); //examine the current character in the string (from left to right)
+    if(currentDigitToken == '.')
     {
-      flag = 1; //set a flag to remember that the character is a decimal point
+      isDecimal = true; //set a flag to remember that the character is a decimal point
     }
     else
     {
     //if the current character is a digit, convert to its
     //int value and include it in the value corresponding to the string.
-      if((c >= '0') && (c<= '9')) 
+      if((currentDigitToken >= '0') && (currentDigitToken<= '9')) 
       {
-        total = total + mult * (c-'0');
+        if(isDecimal) {
+          total = total * 10 + (currentDigitToken-'0');
+        } else {
+          total = total + mult * (currentDigitToken-'0');
+        }
         mult = mult * 10.0;
-        if(flag == 0)
-        {
-         count++; //count the number of digits to the left of a possible decimal point
+        if(isDecimal)
+        { //count the number of digits to the left of a possible decimal point
+          countAfterDec++;
         }
       }
       else
       {
-        if(c == '_') 
+        if(currentDigitToken == '_') 
         {
-          total = -total; //an underscore character represents a negative value
+          isNegative = true;
+          // removed this doesn't make any sense since the negavite sign comes first. total = -total; //an underscore character represents a negative value  
         }
       }
     }
-    p++; //Prepare to move to the next character to the right. 
+    counter++; //Prepare to move to the next character to the right. 
          //This is a private non-static method because it changes the member variable p
-    d = '?';
-    if(p<= e.length()-1)
+    d = ' ';
+    if(counter < expressionInput.length())
     {
-       d = e.charAt(p); //the next character to the left
+       d = expressionInput.charAt(counter); //the next character to the left
     }
-  }while((p >= e.length()-1) && (((d<='9')&&(d>='0'))||(d=='_')||(d=='.')));//check for a valid character
-  if(flag==1)
+  }while((counter < expressionInput.length()) && (((d<='9')&&(d>='0'))||(d=='_')||(d=='.')));//check for a valid character
+  if(isDecimal)
   {
-   total = total/Math.pow(10.0,count*1.0); //compute the value taking into account
+   total = total/Math.pow(10.0,(countAfterDec)*1.0); //compute the value taking into account
                                            //the number of decimal places
+  }
+  if(isNegative) 
+  {
+    total = total * -1;
   }
   return total;
 }
@@ -190,35 +200,35 @@ private double formNum()
 public double evaluator() 
 {
 
-  char token; //current token in the input expression
+  char tempCharacterToken; //current token in the input expression
 
   //loop to scan the string right to left
   do
   {    
-    token = e.charAt(p);
-    if(token == '(')
+    tempCharacterToken = expressionInput.charAt(counter);
+    if(tempCharacterToken == '(')
     {
-        A.addFirst(token); //always push a left parenthesis to delimit a subexpression
-        p++;      
+        operatorQueue.addFirst(tempCharacterToken); //always push a left parenthesis to delimit a subexpression
+        counter++;      
     }
     
     //if the token is a right parenthesis, 
     //evaluate the corresponding subexpression 
-    if(token == ')' )
+    else if(tempCharacterToken == ')' )
     {
       evalDown();
-      p++; //move beyond the right parenthesis
+      counter++; //move beyond the right parenthesis
     }
 
 
     //If the token is an arithmetic operator of higher precedence
-    //than the topmost operator on stack A, push the token onto A.    
-    if((token=='+')||(token=='-')||(token=='*')||(token=='/'))
+    //than the leftmost operator on queue A, add the token onto A.    
+    else if((tempCharacterToken=='+')||(tempCharacterToken=='-')||(tempCharacterToken=='*')||(tempCharacterToken=='/'))
     {          
-      if((A.size() == 0) || (prec(token, A) == true))
+      if((operatorQueue.size() == 0) || (prec(tempCharacterToken, operatorQueue) == true))
       {
-        A.addFirst(token);
-        p++;         
+        operatorQueue.addFirst(tempCharacterToken);
+        counter++;         
       }
       //If the token is an arithmetic operator of lower precedence than that
       //at the top of the stack, then evaluate the operation at the top of stack A.
@@ -230,27 +240,29 @@ public double evaluator()
     
     //if the token is the rightmost  digit of a number or a decimal point on the right,
     //form the number as a double and push onto stack B
-    if(((token<='9')&&(token>='0'))||(token=='.'))
+    else if(((tempCharacterToken<='9')&&(tempCharacterToken>='0'))||(tempCharacterToken=='.')||(tempCharacterToken=='_'))
     {   
-       B.addFirst(formNum());
+       digitQueue.addFirst(formNum());
+    }
+    else {
+      counter++;
     }
  
-  }while(p >= 0);//continue to scan from right to left
+  }while(counter < expressionInput.length());//continue to scan from right to left
 
 
   //after completely scanning the input string, evaluate any remaining operations
-  while(A.size()>0) 
+  while(operatorQueue.size()>0) 
   {
     eval();
   }
   
-  double x = B.remove();
+  double x = digitQueue.remove();
 
   //round the result to 7 places and then display
   expressionOutput((double)Math.round(x*10000000)/10000000.0); 
 
   return x; //return the original double value
 } //end of evaluator
-
 
 } //end of class
--- ExprEval/src/ExprEvaluatorTest2.java
+++ ExprEval/src/ExprEvaluatorTest2.java
@@ -17,14 +17,52 @@ public class ExprEvaluatorTest2
    {
  
     //initialize several expressions, but do not evaluate them immediately
-    ExprEvaluator2 expr1 = new ExprEvaluator2(); //construct an expression via keyboard
-    ExprEvaluator2 expr2 = new ExprEvaluator2("3+4."); //enter an expression via string parameter
+    // ExprEvaluator2 expr1 = new ExprEvaluator2("1+2*3+4"); //construct an expression via keyboard
+    // ExprEvaluator2 expr2 = new ExprEvaluator2("5.1 + .5 * .2 - 4.1"); //enter an expression via string parameter
     ExprEvaluator2 expr3 = new ExprEvaluator2("(3.+4*(1+2*(4+3*(2-_7)))-1)/2.0"); 
+    ExprEvaluator2 expr4 = new ExprEvaluator2("1+2*3+4"); 
+    ExprEvaluator2 expr5 = new ExprEvaluator2("1 + 2 * 3 + 4"); 
+    ExprEvaluator2 expr6 = new ExprEvaluator2("1+9/3-4"); 
+    ExprEvaluator2 expr7 = new ExprEvaluator2("121.75 + 3.25"); 
+    ExprEvaluator2 expr8 = new ExprEvaluator2("4+3*(1+2.*(6/(3+3.))-1)+2.0"); 
+    ExprEvaluator2 expr9 = new ExprEvaluator2("0.1*0.2"); 
+    ExprEvaluator2 expr10 = new ExprEvaluator2("0.011*.022"); 
+    ExprEvaluator2 expr11 = new ExprEvaluator2("_(2-(_1))"); 
+    //ExprEvaluator2 expr13 = new ExprEvaluator2("3.1415927^2"); 
+    //ExprEvaluator2 expr14 = new ExprEvaluator2("2^(2*(2+3))"); 
+    //ExprEvaluator2 expr15 = new ExprEvaluator2("(-3)^(-(3))"); 
+     
+
+    // 1) 2 - (-7) = 9
+    // (27) + 4 = 31
+    // (62) + 1 = 63
+    // (252) + 3 - 1 = 254
+    // 254 / 2 = 127
+
    
     //display the input for expr1
-    System.out.println(expr1.getExpression() + '\n');
-    expr1.evaluator(); //evaluate expr1
+    // System.out.println(expr1.getExpression() + '\n');
 
+    expr3.evaluator(); //evaluate expr1
+    System.out.println(" :should be 127"); 
+    expr4.evaluator(); //evaluate expr1
+    System.out.println(" :should be 11"); 
+    expr5.evaluator(); //evaluate expr1
+    System.out.println(" :should be 11"); 
+    expr6.evaluator(); //evaluate expr1
+    System.out.println(" :should be 0"); 
+    expr7.evaluator(); //evaluate expr1
+    System.out.println(" :should be 125"); 
+    expr8.evaluator(); //evaluate expr1
+    System.out.println(" :should be 12"); 
+    expr9.evaluator(); //evaluate expr1
+    System.out.println(" :should be .02"); 
+    expr10.evaluator(); //evaluate expr1
+    System.out.println(" :should be .000242"); 
+    expr11.evaluator(); //evaluate expr1
+    System.out.println(" :should be -3"); 
+
+    // expr2.evaluator(); //evaluate expr1
   //  expr2.evaluator(); //evaluate expr2; the result will appear as an int value
 
   //  System.out.println(expr3.evaluator()); 
@@ -37,7 +75,41 @@ public class ExprEvaluatorTest2
 
    // expr2 = new ExprEvaluator2("3.1415927*3.1415927"); 
    // expr2.evaluator(); //we can compare (to 7 places) the Java value using pow to the value using evaluator 
-   
+/*
+   1) 1+2*3+4
+
+2) 1 + 2 * 3 + 4
+
+  In the rest of the examples, spacing is optional (within the limits
+
+described in the problem description, e.g.,  3.145 is ok, but not 3  .  14  5, etc.)
+
+3) 1+9/3-4 
+
+4) 121.75 + 3.25
+
+5) 4+3*(1+2.*(6/(3+3.))-1)+2.0
+
+6) 0.1*0.2
+
+7) 0.011*.022
+
+8) -(2-(-1))  (If you don't want to code the logic for -x (negation), where x is an expression,
+
+                 you can enter this as 0 -(2-(-1)), i.e., ordinary subtraction. Some extra credit
+
+                 if your program handles negation ( - as a unary operator).
+
+9) 3.1415927^2
+
+10) 3.14159^1+1
+
+11) 2^(2*(2+3))
+
+12)  (-3)^(-(3)) (or (-3)^(-3), whichever your program can do)
+
+ 13) -(-(-(-1)))) (extra credit if you have coded negation (the unary -).
+   */
   }
 
 }
